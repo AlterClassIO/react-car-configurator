ee4efaa419b22af6075979624a5f0c3665e83812
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-10-23 13:49:29 +0200

    feat: create steps and add them to the Menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,13 +18,83 @@ class App extends React.Component {
     totalPrice: 0
   };
 
+  selectedModel = models.find(model =>
+    model?.key === this.state.config?.model
+  );
+
+  steps = [
+    {
+      name: "car",
+      settings: [
+        {
+          label: "Select car",
+          type: "text",
+          prop: "model",
+          options: models.map(model => ({
+            value: model.key,
+            label: model.name
+          }))
+        },
+        {
+          label: "Select type",
+          type: "text",
+          prop: "car_type",
+          options: this.selectedModel?.types ?? [],
+          disclaimer_1: "All cars have Dual Motor All-Wheel Drive, adaptive air suspension, premium interior and sound.",
+          disclaimer_2: "Tesla All-Wheel Drive has two independent motors that digitally control torque to the front and rear wheels—for far better handling and traction control. Your car can drive on either motor, so you don't need to worry about getting stuck on the road."
+        }
+      ]
+    },
+    {
+      name: "exterior",
+      settings: [
+        {
+          label: "Select color",
+          type: "color",
+          prop: "color",
+          options: this.selectedModel?.colors ?? []
+        },
+        {
+          label: "Select wheels",
+          type: "image",
+          prop: "wheels",
+          options: this.selectedModel?.wheels ?? []
+        }
+      ]
+    },
+    {
+      name: "interior",
+      settings: [
+        {
+          label: "Select premium interior",
+          type: "text",
+          prop: "interior_color",
+          options: this.selectedModel?.interiorColors ?? []
+        },
+        {
+          label: "Select interior layout",
+          type: "text",
+          prop: "interior_layout",
+          options: this.selectedModel?.interiorLayouts ?? []
+        },
+      ]
+    },
+    {
+      name: "summary"
+    }
+  ];
+
+  goToStep = (step) => {
+    this.setState({ currentStep: step });
+  };
+
   render() {
     return (
       <div className="app">
         <Menu
-          items={[]}
+          items={this.steps.map(step => step.name)}
           selectedItem={this.state.currentStep}
-          onSelectItem={null}
+          onSelectItem={this.goToStep}
         />
       </div>
     );
